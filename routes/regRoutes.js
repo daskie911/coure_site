173091b6d992f5f2afff38adaf4bf15f9686f623
yawn911 <darkpunky94@gmail.com>
2024-03-04 17:42:41 +0100

    Add bcryptjs dependency and implement user registration

--- routes/regRoutes.js
+++ routes/regRoutes.js
@@ -1,34 +1,31 @@
+const bcrypt = require("bcryptjs");
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
 
 router.get("/", (req, res) => {
   res.render("reg");
 });
 
-router.post("/register", (req, res) => {
-  let { email, password, password2 } = req.body;
-  console.log(email, password, password2);
-  console.log(parseData);
+router.post("/", async (req, res) => {
+  try {
+    const { username, password, email } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const candidate = await User.findOne({ email });
 
-  if (password !== password2) {
-    res.render("reg");
-    return;
-  }
-
-  let userExists = false;
-
-  for (let user of parseData.users) {
-    if (user.email === email) {
-      userExists = true;
-      break;
+    if (candidate) {
+      return res.redirect("/");
+    } else {
+      const user = new User({
+        username,
+        password: hashedPassword,
+        email,
+      });
+      await user.save();
+      return res.redirect("/");
     }
-  }
-
-  if (userExists) {
-    res.send("User already exists!");
-    res.redirect("/");
-  } else {
-    res.redirect("/");
+  } catch (error) {
+    console.log(error);
   }
 });
 
